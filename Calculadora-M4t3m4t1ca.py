9d5ad3939e2c11592501be3c282c076249af45fb
AxlAndrade <83742076+AxlAndrade@users.noreply.github.com>
2021-05-06 21:32:25 -0300

    2ª versão da m4t3m4t1c4-v2
    Implementada a opção de calculadora de equações (no momento, só está disponível equações de 1º grau, 2º grau é WIP).
    Organização e melhoria na definição das funções.

--- Calculadora-M4t3m4t1ca.py
+++ Calculadora-M4t3m4t1ca.py
@@ -1,3 +1,5 @@
+#Funções da Calculadora básica
+
 def soma(): #Função soma
     soma = eval(input("Digite na forma a + b + ... + n os valores que você quer somar\n"))
     print(soma)
@@ -14,19 +16,12 @@ def divisao(): #Função divisão
     divisao = eval(input("Digite na forma a/b os valores que você quer dividir\n"))
     print(float(divisao))
 
-def expressoes():
+def expressoes(): #Função expressão
     expressao = eval(input("Digite aqui a expressão que você deseja calcular.\n"))
     print(float(expressao))
 
-def retornar_menu():
-    mais_op = int(input("Você deseja realizar mais alguma operação ?\n Sim (1)\n Não (0)\n"))
-    if mais_op == 1:
-        menu()
-    else:
-        exit()
-
-def menu_basico():
-    menu_bas = int(input("Selecione qual operação você quer utilizar\n 1. Soma\n 2. Subtração\n 3. Multiplicação\n 4. Divisao\n 5. Expresões\n"))
+def menu_basico(): #Menu da calculadora básica
+    menu_bas = int(input("Selecione qual operação você quer utilizar\n 1. Soma\n 2. Subtração\n 3. Multiplicação\n 4. Divisao\n 5. Expressões\n"))
     if menu_bas == 1:
         soma()
         retornar_menu()
@@ -57,10 +52,43 @@ def menu_basico():
     else:
         pass
 
+def retornar_menu(): #Função para retornar ao menu(menu())
+    mais_op = int(input("Você deseja realizar mais alguma operação ?\n Sim (1)\n Não (0)\n"))
+    if mais_op == 1:
+        menu()
+    else:
+        exit()
+
+#Funções da Calculadora de Equações
+
+from sympy import symbols, Eq, solve
+
+def eq_1_grau():
+    x = symbols('x')
+    expr = input("Digite a sua equação na forma 'a * x + b'\n")
+    sol = solve(expr)
+    print(sol)
+    retornar_menu()
+
+def menu_eq():
+    opcao_eq = int(input("Selecione qual tipo de equação você deseja resolver\n 1. Equação de 1º grau\n"))
+    if opcao_eq == 1:
+        eq_1_grau()
+
+
+#Menu principal do programa
+
 def menu(): #Função menu
-    opcao = int(input("Selecione qual funcção da calculadora você quer utilizar:\n 0. Sair\n 1. Básica\n"))
+    opcao = int(input("Selecione qual funcção da calculadora você quer utilizar:\n 0. Sair\n 1. Básica\n 2. Calculadora de equações\n"))
     if opcao == 1:
         print("Você selecionou a calculadora básica.")
-        menu_basico()  
+        menu_basico()
+    else:
+        pass
+
+    if opcao == 2:
+        print("Você selecionou a calculadora de equações")
+        menu_eq()
+
 
 menu()

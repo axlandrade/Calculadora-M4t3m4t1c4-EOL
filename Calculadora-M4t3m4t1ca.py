2faa63bc6210b333e1e6b9c3b87ec40ea88c3dc5
Axl Andrade <83742076+AxlAndrade@users.noreply.github.com>
2021-05-07 19:56:20 -0300

    Adicionado retornar_menu()
    Adicionado a função retornar_menu() na "nova" calculadora básica e adicionado comentários no código

--- Calculadora-M4t3m4t1ca.py
+++ Calculadora-M4t3m4t1ca.py
@@ -6,6 +6,7 @@ def funcao_basica(): #Menu da calculadora básica
     print("Aqui nessa função você pode realizar operações como:\n 1. Soma (a+b)\n 2. Subtração (a-b)\n 3. Multiplicação (a*b)\n 4. Divisão (a/b)\n")
     funcao_bas = eval(input("Digite aqui a sua operação matemática.\n"))
     print(float(funcao_bas))
+    retornar_menu()
 
 import time
 
@@ -22,14 +23,14 @@ def retornar_menu(): #Função para retornar ao menu(menu())
 
 from sympy import symbols, Eq, solve
 
-def eq_1_grau():
+def eq_1_grau(): #Função para resolver equações do 1º grau
     x = symbols('x')
     expr = input("Digite a sua equação na forma 'a * x + b'\n")
     sol = solve(expr)
     print(sol)
     retornar_menu()
 
-def menu_eq():
+def menu_eq(): #Menu da calculadora de equações
     opcao_eq = int(input("Selecione qual tipo de equação você deseja resolver\n 0. Voltar ao menu\n 1. Equação de 1º grau\n"))
     if opcao_eq == 0:
         menu()

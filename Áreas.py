457da9f08991a166e95850fa02097ead7fecb620
Axl Andrade <83742076+AxlAndrade@users.noreply.github.com>
2021-05-10 00:27:54 -0300

    Áreas
    Como a calculadora vai ter uma função de calcular áreas de figuras geométricas, irei definir as funções que vão representar cada área nesse arquivo.

---
+++ Áreas.py
@@ -0,0 +1,6 @@
+def area_do_quadrado():
+    lado = int(input("Insira o lado do seu quadrado\n"))
+    area_quadrado = int(lado**2)
+    print("A área do seu quadrado vale:", area_quadrado)
+
+area_do_quadrado()
